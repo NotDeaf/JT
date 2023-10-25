75e1994d52f50d73bb5531cc7fd0537a51f01d4b
Luke P <36600857+NotDeaf@users.noreply.github.com>
2023-10-25 19:01:13 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,31 @@
-let hasZoomed = false;
+let startY;
 
-window.addEventListener('wheel', function(event) {
+// Existing wheel event listener for desktop
+window.addEventListener('wheel', handleScroll);
+
+// New touchmove event listener for mobile
+window.addEventListener('touchstart', (event) => {
+    startY = event.touches[0].clientY;
+});
+
+window.addEventListener('touchmove', (event) => {
+    const endY = event.changedTouches[0].clientY;
+    const distance = startY - endY;
+    handleScroll({ deltaY: distance });
+});
+
+function handleScroll(event) {
     if (!hasZoomed) {
         const image = document.getElementById("coverImage");
 
-        // Detect the direction of the scroll
+        // Detect the direction of the scroll/swipe
         const scrollDirection = event.deltaY;
 
-        if (scrollDirection > 0) {  // If scrolling down
+        if (scrollDirection > 0) {  // If scrolling/swiping down
             // Zoom into a specific point
             image.style.transition = "transform 1s, filter 1s";
             image.style.transform = "translate(-50%, -50%) scale(15)";
             image.style.filter = "blur(10px)"; /* Adjust this value to control the amount of blur */
-
 
             // After zoom effect completes, hide the cover and show content
             setTimeout(() => {
@@ -24,4 +37,4 @@ window.addEventListener('wheel', function(event) {
             hasZoomed = true;  // Prevent the event from triggering again
         }
     }
-});
+}
